cf1a4f8060f694dd56b9c5f81c92a88d99b53be8
0-virus <pyk09667@gmail.com>
2026-03-23 02:39:19 +0900

    메인 페이지 구성 api 개발 완료

--- app/api/blogs/[blogId]/route.ts
+++ app/api/blogs/[blogId]/route.ts
@@ -8,15 +8,24 @@ export async function GET(
   try {
     // URL에서 쿼리 파라미터 가져오기
     const { searchParams } = new URL(request.url);
+    const categoryId = searchParams.get("category_id") || null;
     const page = parseInt(searchParams.get("page") || "1");
     const limit = parseInt(searchParams.get("limit") || "5");
     const skip = (page - 1) * limit;
 
     // Path Parameter 불러오기
     const { blogId } = await params;
-    if (!blogId) {
+
+    // 파라미터 유효성 검사
+    if (isNaN(Number(blogId))) {
       return NextResponse.json(
-        { message: "Path Parameter가 유효하지 않습니다." },
+        { message: "blogId가 유효하지 않습니다." },
+        { status: 400 },
+      );
+    }
+    if (categoryId !== null && isNaN(Number(categoryId))) {
+      return NextResponse.json(
+        { message: "categoryId가 유효하지 않습니다." },
         { status: 400 },
       );
     }
@@ -28,6 +37,8 @@ export async function GET(
         select: {
           title: true,
           posts: {
+            where:
+              categoryId === null ? {} : { category_id: BigInt(categoryId) },
             orderBy: { published_at: "desc" },
             skip: skip,
             take: limit,
@@ -38,7 +49,12 @@ export async function GET(
           },
         },
       }),
-      prisma.posts.count({ where: { blog_id: BigInt(blogId) } }),
+      prisma.posts.count({
+        where: {
+          blog_id: BigInt(blogId),
+          ...(categoryId === null ? {} : { category_id: BigInt(categoryId) }),
+        },
+      }),
     ]);
     if (!data) {
       return NextResponse.json(
@@ -46,12 +62,10 @@ export async function GET(
         { status: 404 },
       );
     }
-    const totalCount = data.posts.length;
 
     return NextResponse.json(
       {
         message: "블로그 메인 정보 조회 성공!",
-
         postList: {
           pagination: {
             page,
@@ -66,6 +80,9 @@ export async function GET(
     );
   } catch (error: unknown) {
     console.error("[GET api/blogs/[blogId]]", error);
-    return NextResponse.json({ message: "조회 실패..." }, { status: 500 });
+    return NextResponse.json(
+      { message: "블로그 메인 정보 조회 실패..." },
+      { status: 500 },
+    );
   }
 }
